dbd473282177e9976ca29f132ae0e8d57a49b215
3dw1nM0535 <workockmoses@gmail.com>
2017-10-18 22:35:38 +0300

    defined logic route for Oauth

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,17 +2,56 @@ var path = process.cwd();
 
 var ClickHandler= require(path + '/app/controllers/clickHandler.server');
 
-//Home route definition and api 
 
-module.exports = function (app) {
+module.exports = function (app, passport) {
+
+  function isLoggedIn (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    } else {
+      res.redirect('/login');
+    }
+  }
 
   var clickHandler = new ClickHandler();
 
-  app.route('/').get(function (req, res) {
+  //default route
+  app.route('/').get(isLoggedIn, function (req, res) {
     res.sendFile(path + '/public/index.html');
   });
 
-  app.route('/api/clicks').get(clickHandler.getClicks)
-                          .post(clickHandler.addClicks)
-                          .delete(clickHandler.resetClicks);
+    //login route
+  app.route('/login').get(function (req, res) {
+    res.sendFile(path + '/public/login.html');
+  });
+
+  //logout route
+  app.route('/logout').get(function (req, res) {
+    req.logout();
+    res.redirect('/login');
+  });
+
+  //user profile route
+  app.route('/profile').get(isLoggedIn, function (req, res) {
+    res.sendFile(path + '/public/profile.html');
+  });
+
+  //user api route
+  app.route('/api/:id').get(isLoggedIn, function (req, res) {
+    res.json(req.user.github);
+  });
+
+  //authenticate route
+  app.route('/auth/github').get(passport.authenticate('github'));
+
+  //callback authenticate url
+  app.route('/auth/github.callback').get(passport.authenticate('github', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  }));
+
+  //api route handler
+  app.route('/api/:id/clicks').get(isLoggedIn, clickHandler.getClicks)
+                              .post(isLoggedIn, clickHandler.addClicks)
+                              .delete(isLoggedIn, clickHandler.resetClicks);
 };
