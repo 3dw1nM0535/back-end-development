eab93e27b6ea2b412b9ad5c4e977c243f1ef7434
3dw1nM0535 <workockmoses@gmail.com>
2017-10-18 16:19:54 +0300

    Added middleware to server static files

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 //Import express module to build server basing on the module
 var express = require('express'); 
+var routes = require('./app/routes/index');
 
 //Start the express module
 var app = express();
 
-//Get home route
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/index.html' );
-});
+//Static file Middleware
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+
+routes(app);
 
 //Server init
 app.listen(8080, function () {
