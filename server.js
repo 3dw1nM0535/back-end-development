461d059bcb3349afa9a766bca7dab432228f5496
3dw1nM0535 <workockmoses@gmail.com>
2017-10-18 16:33:18 +0300

    wrapped every instance in mongo.connect()

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 //Import express module to build server basing on the module
 var express = require('express'); 
 var routes = require('./app/routes/index');
+var mongo = require('mongodb').MongoClient;
+
+var url = 'mongodb://localhost:27017/clementinejs';
 
 //Start the express module
 var app = express();
 
-//Static file Middleware
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+mongo.connect(url, function (err, db) {
+  if(err) {
+    throw new Error('Database failed to connect!');
+  } else {
+    console.log('Coonection to databse was successfull!');
+  }
 
-routes(app);
+  //Static file Middleware
+  app.use('/public', express.static(process.cwd() + '/public'));
+  app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-//Server init
-app.listen(8080, function () {
-  console.log('Server running on port 8080!');
+  routes(app, db);
+  
+  //Server init
+  app.listen(8080, function () {
+    console.log('Server running on port 8080!');
+  });
+  
 });
+
