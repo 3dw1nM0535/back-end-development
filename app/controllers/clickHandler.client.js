5f4109437a12c2557a45c95a338762a381f76f54
3dw1nM0535 <workockmoses@gmail.com>
2017-10-19 00:54:44 +0300

    redefined ajax calls to common/ajax-functions.js

--- app/controllers/clickHandler.client.js
+++ app/controllers/clickHandler.client.js
@@ -5,35 +5,8 @@
   var addButton = document.querySelector('.btn-add');
   var deleteButton = document.querySelector('.btn-delete');
   var clickNbr = document.querySelector('#click-nbr');
-  var apiURI = 'http://localhost:8080/api/clicks';
+  var apiURI = appURI + '/api/:id/clicks';
 
-  //function to listen for 'DOMContentLoaded' event
-  function ready (fn) {
-    if (typeof fn !== 'function') {
-      return;
-    }
-
-    if (document.readyState === 'complete') {
-      return fn();
-    }
-
-    document.addEventListener('DOMContentLoaded', fn, false);
-
-  }
-
-  //AJAX request to the API
-  function ajaxRequest (method, url, callback) {
-    var xmlhttp = new XMLHttpRequest();
-
-    xmlhttp.onreadystatechange = function () {
-      if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-        callback(xmlhttp.response);
-      }
-    };
-
-    xmlhttp.open(method, url, true);
-    xmlhttp.send();
-  }
 
   //Update element with data from API
   function updateClickCount (data) {
@@ -42,19 +15,19 @@
   }
 
   //Execute ready()
-  ready (ajaxRequest('GET', apiURI, updateClickCount));
+  ajaxFunctions.ready (ajaxFunctions.ajaxRequest('GET', apiURI, updateClickCount));
 
   //Add event listener to post clicks to API and update the DOM content
   addButton.addEventListener('click', function() {
-    ajaxRequest('POST', apiURI, function () {
-      ajaxRequest('GET', apiURI, updateClickCount);
+    ajaxFunctions.ajaxRequest('POST', apiURI, function () {
+      ajaxFunctions.ajaxRequest('GET', apiURI, updateClickCount);
     });
   }, false);
 
   //Add event listener to delete clicks from API and update the DOM content
   deleteButton.addEventListener('click', function () {
-    ajaxRequest('DELETE', apiURI, function () {
-      ajaxRequest('GET', apiURI, updateClickCount);
+    ajaxFunctions.ajaxRequest('DELETE', apiURI, function () {
+      ajaxFunctions.ajaxRequest('GET', apiURI, updateClickCount);
     });
   }, false);
 
