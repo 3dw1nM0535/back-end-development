c9464b39bf0116c27b8b75f2e1f0acb05c4e4ab5
3dw1nM0535 <workockmoses@gmail.com>
2017-10-18 19:13:27 +0300

    redefined database connect with mongoose.connect()

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
 //Import express module to build server basing on the module
 var express = require('express'); 
 var routes = require('./app/routes/index');
-var mongo = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
 
 var url = 'mongodb://localhost:27017/clementinejs';
 
 //Start the express module
 var app = express();
 
-mongo.connect(url, function (err, db) {
-  if(err) {
-    throw new Error('Database failed to connect!');
-  } else {
-    console.log('Connection to databse was successfull!');
-  }
+//Connect to database
+mongoose.connect(url, { useMongoClient: true });
 
-  //Static file Middleware
-  app.use('/public', express.static(process.cwd() + '/public'));
-  app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+//Static file Middleware
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-  routes(app, db);
-  
-  //Server init
-  app.listen(8080, function () {
-    console.log('Server running on port 8080!');
-  });
-  
+routes(app, db);
+
+//Server init
+app.listen(8080, function () {
+  console.log('Server running on port 8080!');
 });
+
+
 
