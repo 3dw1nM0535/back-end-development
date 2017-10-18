6ec830f88528113d4ac846c40b34a294fd6bb465
3dw1nM0535 <workockmoses@gmail.com>
2017-10-18 17:44:12 +0300

    store client component in vars for logic implementation

---
+++ app/controllers/clickHandler.client.js
@@ -0,0 +1,8 @@
+//Handle user interactivity between the browser and the API
+
+(function() {
+  var addButton = document.querySelector('.btn-add');
+  var deleteButton = document.querySelector('.btn-delete');
+  var clickNbr = document.querySelector('#click-nbr');
+  var apiURI = 'http://127.0.0.1:8080/api/clicks';
+})();
