7d874f8ce36c5a22db13145a216cb2984d974fd6
3dw1nM0535 <workockmoses@gmail.com>
2017-10-18 22:33:28 +0300

    user data model Schema

---
+++ app/models/user.js
@@ -0,0 +1,18 @@
+//User data model schema
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  github: {
+    id: String,
+    displayName: String,
+    username: String,
+    publicRepos: Number
+  },
+  nbrClicks: {
+    clicks: Number
+  }
+});
+
+
+module.exports = mongoose.model('User', UserSchema);
